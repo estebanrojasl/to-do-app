cd4cebd05c34c15770bafb20ba757ad22805c2c7
esteban7590 <esteban7590@hotmail.com>
2020-08-27 11:56:26 -0500

    starting project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My to-do List</h1>
+      <input type="checkbox" name="task1" id="" />
+      <label htmlFor="task1">Code</label>
+      <br />
+      <input type="checkbox" name="task2" id="" />
+      <label htmlFor="task2">Eat</label>
+      <br />
+      <input type="checkbox" name="task3" id="" />
+      <label htmlFor="task3">Sleep</label>
+      <br />
+      <input type="checkbox" name="task4" id="" />
+      <label htmlFor="task4">Repeat</label>
     </div>
   );
-}
+};
 
 export default App;
