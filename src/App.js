ce4d03ac902b3628567a2b771cc4149c84dfe3a3
esteban7590 <esteban7590@hotmail.com>
2020-08-30 14:08:55 -0500

    checking with event handlers and set state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = { data: ToDosData };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      return prevState.data.map((item) => {
+        if (item.id === id) {
+          item.completed = !item.completed;
+        }
+        return item;
+      });
+    });
+  }
+
   render() {
     const toDos = this.state.data.map((item) => (
-      <ToDoItem key={item.id} item={item} />
+      <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
     return <div className="to-do-list">{toDos}</div>;
   }
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,11 @@ import React from "react";
 const ToDoItem = (props) => {
   return (
     <div className="to-do-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input
+        type="checkbox"
+        onChange={() => props.handleChange(props.item.id)}
+        checked={props.item.completed}
+      />
       <label>{props.item.text}</label>
       <br />
     </div>
